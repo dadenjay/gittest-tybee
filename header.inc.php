a39b316a586ad6f77aeeba921fa9f1705b04eafa
Nathan <njh246@gmail.com>
2012-02-29 09:29:20 -0500

    second try to do initial save

---
+++ header.inc.php
@@ -0,0 +1,30 @@
+
+<link rel="shortcut icon" href="http://orderlydrawer.com/wp-content/uploads/2009/02/favicon.ico" >
+	<link href="../css/maincmjr800px.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<div id="outerDiv">
+
+		<div id="headerBox">
+			<div id="logo">
+				<a href="http://orderlydrawer.com"><img src="http://orderlydrawer.com/wp-content/uploads/2011/05/odlogo1.jpg" title="drawer organizers" alt="drawer organizers"></a>
+			</div>			
+		
+			<div id="viewCartBox">
+				<a href="http://www.e-junkie.com/ecom/gb.php?c=cart;cl=69858;ejc=2"  target="ej_ejc" class="ec_ejc_thkbx"><img src="http://www.e-junkie.com/ej/ej_view_cart.gif" border="0" alt="View Cart"/></a>
+				<p>423.521.5357</p>
+			</div>			
+			<div id = "mainMenu">
+
+					<ul>
+						<li><a href="http://orderlydrawer.com">Home</a></li>
+						<li><a href="http://orderlydrawer.com/custom-organizers">Custom Inserts</a></li>
+
+						<li><a href="http://orderlydrawer.com/standard-organizers">Standard Organizers</a></li>
+						<li><a href="http://orderlydrawer.com/about-us">About Us</a></li>
+						<li><a href="http://orderlydrawer.com/contact-us">Contact</a></li>
+
+						<li><a href="http://orderlydrawer.com/customers-comment">Testimonials</a></li>
+					</ul>
+			</div>		
+		</div>
